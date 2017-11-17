e2f3d69d495bd8399a4fe6125b5bb68293224411
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-17 23:35:16 +1100

    First commit

---
+++ booleanValues_003/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace booleanValues_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // boolean values are true or false
+            bool myFirstBoolean = 3 + 2 == 5;
+            Console.WriteLine(myFirstBoolean.ToString());
+
+            bool mySecondBoolean = 3 + 2 > 5;
+            Console.WriteLine(mySecondBoolean.ToString());
+
+            // && requires both sides to be true in order to be true
+            bool bothAreTrue = myFirstBoolean && mySecondBoolean;
+            Console.WriteLine("bothAreTrue evaluates " + bothAreTrue);
+
+            // || or requires either side to be true in order to be true
+            bool oneIsTrue = myFirstBoolean || mySecondBoolean;
+            Console.WriteLine("oneIsTrue evaluates {0} because one " +
+                              "expression is true", oneIsTrue);
+            // ! not reverses truth
+            bool reverseMyFirstBoolean = !myFirstBoolean;
+            Console.WriteLine("myFirstBoolean is {0} but " +
+                              "reverseMyFirstBoolean is {1}",
+                              myFirstBoolean, reverseMyFirstBoolean);
+        }
+    }
+}
+ 
